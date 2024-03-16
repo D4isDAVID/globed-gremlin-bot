bb0bfe520b91f2e02ecbaefcc2db244b8c67d7e7
David Malchin <malchin459@gmail.com>
2024-03-16 02:20:40 +0200

    Fix the config timestamp display sometimes being invalid

--- src/components/gremlins/utils/daily-timestamp.ts
+++ src/components/gremlins/utils/daily-timestamp.ts
@@ -3,5 +3,8 @@ import { TimestampStyles, time } from '@discordjs/formatters';
 export const constantTimeDisplay = (hour: number, minute: number): string =>
     `${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}`;
 
-export const timestampDisplay = (hour: number, minute: number): string =>
-    time(hour * 60 * 60 + minute * 60, TimestampStyles.ShortTime);
+export const timestampDisplay = (hour: number, minute: number): string => {
+    const today = new Date();
+    today.setUTCHours(hour, minute, 0, 0);
+    return time(Math.floor(today.getTime() / 1000), TimestampStyles.ShortTime);
+};
